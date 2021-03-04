e7833c368a144c0f69bdd2676656d50ca1c361f0
elodiebhs <elodiebouthors@hotmail.com>
2021-03-04 23:58:56 +0000

    update getAllProperties function with queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -110,49 +110,49 @@ exports.getAllReservations = getAllReservations;
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
+  WHERE 1 = 1
   `;
 
   // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += `AND city LIKE $${queryParams.length} `;
     
   }
   
-
+   //if an owner_id is passed in, only return properties belonging to that owner.
   if(options.owner_id){
-    if(queryParams.length === 0){
-      queryString+= 'WHERE ';
-    } else {
-      queryString+= 'AND ';
-    }
-   
+  
     queryParams.push(`${options.owner_id}`);
-    queryString += `owner_id = $${queryParams.length} `;
-   console.log("test", queryString)
+    queryString += `AND owner_id = $${queryParams.length} `;
+    //console.log("test", queryString)
    }
-
    
+   //if a minimum_price_per_night and a maximum_price_per_night, only return properties within that price range.
+   if(options.minimum_price_per_night){
+    queryParams.push(`${options.minimum_price_per_night*100}`);
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+  }
+  
 
-  // if(options.minimum_price_per_night){
-  //   queryParams.push
-  //   queryString +=
-  // }
+  if(options.minimum_price_per_night){
+    queryParams.push(`${options.maximum_price_per_night*100}`);
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+  }
 
-  // if(options.maximum_price_per_night){
-  //   queryParams.push
-  //   queryString +=
-  // }
+  queryString += `GROUP BY properties.id\n` 
 
-  // if(options.minimu_rating){
-  //   queryParams.push(`%${options.minimu_rating}%`);
-  //   queryString += `HAVING AVG(property_reviews.rating) >= `;
-  // }
+//if a minimum_rating is passed in, only return properties with a rating equal to or higher than that
+
+if(options.minimum_rating){
+  queryParams.push(`${options.minimum_rating}`);
+  queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length} `;
+}
+
 
   // 4
   queryParams.push(limit);
   queryString += `
-  GROUP BY properties.id
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
